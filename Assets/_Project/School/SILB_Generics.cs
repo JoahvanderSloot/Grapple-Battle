f495aede5e1a0140bef54cbf17a922f1e44384f6
JoahvanderSloot <144343776+JoahvanderSloot@users.noreply.github.com>
2024-07-20 11:55:51 +0200

    help!!

---
+++ Assets/_Project/School/SILB_Generics.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SILB_Generics<T> : MonoBehaviour where T : Component
+{
+    private static T m_instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (Instance == null)
+            {
+                m_instance = FindObjectOfType<T>();
+                if (m_instance == null)
+                {
+                    GameObject _obj = new GameObject();
+                    _obj.name = typeof(T).Name;
+                    m_instance = _obj.AddComponent<T>();
+                }
+            }
+            return m_instance;
+        }
+    }
+    
+    public virtual void Awake()
+    {
+        if (m_instance == null)
+        {
+            m_instance = this as T;
+
+            //keeps it when loading new scene (optional)
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
