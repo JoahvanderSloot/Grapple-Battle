a2ab1b91d246dcc30986e3427b37ed9d74c815ae
JoahvanderSloot <144343776+JoahvanderSloot@users.noreply.github.com>
2024-09-25 10:42:15 +0200

    miooi gemaakt

--- Assets/_Project/Scrips/Knockback.cs
+++ Assets/_Project/Scrips/Knockback.cs
@@ -5,19 +5,9 @@ public class Knockback : HitPoints
     private int oldHitPoints;
     public Vector3 kbDirection;
 
-    private void Start()
-    {
-        oldHitPoints = HP;
-    }
-
     private void Update()
     {
         DestroyOnKill();
-
-        if(HP < oldHitPoints)
-        {
-            oldHitPoints = HP;
-        }
     }
 
     public void AddKnockback(float knockBackStrength)
